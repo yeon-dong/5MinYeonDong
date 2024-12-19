5940646d1b9ab5dd32264590ae53829053e82d2f
jsj <jsj@jsjs-iMac.local>
2024-12-19 10:27:00 +0900

    init commit

---
+++ fiveMin/fiveMin/IssueChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IssueChatViewController.swift
+//  fiveMin
+//
+//  Created by jsj on 12/19/24.
+//
+
+import UIKit
+
+class IssueChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
